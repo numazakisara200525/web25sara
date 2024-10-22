4cb815a86cddb17ed05c4cf88737459b08023dc1
numazakisara200525 <167832970+numazakisara200525@users.noreply.github.com>
2024-10-22 10:05:41 +0900

    Update ex6-5.js

--- ex6-5.js
+++ ex6-5.js
@@ -4,3 +4,4 @@ function taro65() {who65 = '太郎';}
 function hanako65() {who65 = '花子';}
 function taro66(){alert('太郎が来た');}
 function hanako66(){alert('花子が来た');}
+function taro66() {alert(who65 + 'が来た');}
