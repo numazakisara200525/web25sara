4fda644b540361ed94f4d83897996c03a4f718b1
numazakisara200525 <167832970+numazakisara200525@users.noreply.github.com>
2024-10-29 11:38:41 +0900

    Update ex6-88.js

--- ex6-88.js
+++ ex6-88.js
@@ -1,2 +1,1 @@
 function gazo688() {document.getElementById('who688').innerHTML='<img src="./image/正しい制服.png" alt="オン">’;}
-function gazo688() {document.getElementById('who688').innerHTML='<img src="./image/制服.png" alt="オン">’;}
