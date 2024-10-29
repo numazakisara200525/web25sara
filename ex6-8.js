da52599ca0409c1494ec743e549460bd1c667ab3
numazakisara200525 <167832970+numazakisara200525@users.noreply.github.com>
2024-10-29 10:17:27 +0900

    Update ex6-8.js

--- ex6-8.js
+++ ex6-8.js
@@ -1,4 +1,2 @@
 function sayhello68() {alert('Hello, ' + document.getElementById('who68').innerHTML + '!');}
 function taro68() {document.getElementById('who68').innerHTML = '太郎';}
-function hanako68() {document.getElementById('who68').innerHTML = '花子';}
-function gazo688() {document.getElementById('who688').innerHTML='<img src="./image/正しい制服.png" alt="オン">’;}
