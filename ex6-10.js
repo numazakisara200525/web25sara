6bc49d276fbc98d65dffe9fc334ebcca77b94180
numazakisara200525 <167832970+numazakisara200525@users.noreply.github.com>
2024-11-12 10:04:59 +0900

    Update ex6-10.js

--- ex6-10.js
+++ ex6-10.js
@@ -1,2 +1,2 @@
-function imgchange1() {document.getElementById('logo').src = './image/城西大学ロゴ.jpg';}
-function imgchange1() {document.getElementById('modosu').src = './image/城西大学ロゴ.jpg';}
+function imgchange1() {document.getElementById('logo').src = './image/城西大学.jpg';}
+function imgchange1() {document.getElementById('modosu').src = './image/城西大学.jpg';}
