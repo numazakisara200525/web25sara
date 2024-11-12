5129a4b50b437fbac900fffe8682e46e46244aec
numazakisara200525 <167832970+numazakisara200525@users.noreply.github.com>
2024-11-12 13:37:16 +0900

    Update ex6-10.js

--- ex6-10.js
+++ ex6-10.js
@@ -1,2 +1,2 @@
 function imgchange1() {document.getElementById('logo').src = './image/josai.jpg';}
-function imgchange2() {document.getElementById('modosu').src = './image/logo.jpg';}
+function imgchange2() {document.getElementById('logo').src = './image/logo.jpg';}
