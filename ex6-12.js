73550cbbb193c123fdbf56aea13f1dc73d86ddad
numazakisara200525 <167832970+numazakisara200525@users.noreply.github.com>
2024-11-19 10:44:29 +0900

    Update ex6-12.js

--- ex6-12.js
+++ ex6-12.js
@@ -1,6 +1,13 @@
 function hyoji() {
+//表示する文字
 var str = "一文字ずつ表示します。"; 
+//テキストボックスの文字数
 var cnt = document.timer.moji.value.length;
+//文字が全部表示されているか確認
 if ( cnt < 11 ) {document.timer.moji.value = str.substr(0,cnt+1); }
+//現在より１文字多く切り出して表示
 else {document.timer.moji.value = ""; } }
-function startfnc() {setInterval("hyoji()",1000); }
+//全て表示されたら、空文字に戻す
+function startfnc() {
+//関数hyouji()を1000ミリ秒間隔で呼び出す
+setInterval("hyoji()",1000); }
