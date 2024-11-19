157d7741f8f3299824ea463123e2c3ea39380f7a
numazakisara200525 <167832970+numazakisara200525@users.noreply.github.com>
2024-11-19 10:21:32 +0900

    Update ex6-11.js

--- ex6-11.js
+++ ex6-11.js
@@ -1,2 +1,2 @@
-function imgchange3() {document.getElementById('tou').src = './image/tou.jpg';}
-function imgchange4() {document.getElementById('tou').src = './image/tou2.jpg';}
+function imgchange3() {document.getElementById('tou').src = './image/tou2.jpg';}
+function imgchange4() {document.getElementById('tou').src = './image/tou.jpg';}
