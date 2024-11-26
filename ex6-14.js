27b607995d16f19a352fbb8b09f893eb3018de38
numazakisara200525 <167832970+numazakisara200525@users.noreply.github.com>
2024-11-26 11:53:05 +0900

    Update ex6-14.js

--- ex6-14.js
+++ ex6-14.js
@@ -1,3 +1,3 @@
-function hyoji14() { imgchange5(); setTimeout(hyoji14, 3000); } 
+function hyoji14() { imgchange5(); setTimeout(imgchange6, 3000); } 
 function imgchange5() { document.getElementById('tou2').src = './image/tou2.jpg';}
 function imgchange6() { document.getElementById('tou2').src = './image/tou.jpg';}
