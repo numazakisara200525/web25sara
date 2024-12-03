5b9b849a5cfb60236ef72c7455472b2ee3218ff0
numazakisara200525 <167832970+numazakisara200525@users.noreply.github.com>
2024-12-03 10:33:09 +0900

    Update ex7-3.js

--- ex7-3.js
+++ ex7-3.js
@@ -1,1 +1,5 @@
-function calculateBMI() { var weight = Number(document.getElementById('weight').value); var height = Number(document.getElementById('height').value) / 100; // cmをmに変換 var bmi = weight / (height * height); alert("あなたのBMIは: " + bmi.toFixed(2) + " です"); }
+function calculateBMI() { 
+var 体重 = Number(document.getElementById('weight').value); 
+var 身長 = Number(document.getElementById('height').value) / 100;
+// cmをmに変換 
+var BMI = 体重 / (身長 * 身長); alert("あなたのBMIは: " + bmi.toFixed(2) + " です"); }
