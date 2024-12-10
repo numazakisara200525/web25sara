6b4464ea3f2556fcdb73d3318939c80651eec9f2
numazakisara200525 <167832970+numazakisara200525@users.noreply.github.com>
2024-12-10 09:28:45 +0900

    Update ex7-3.js

--- ex7-3.js
+++ ex7-3.js
@@ -1,5 +1,4 @@
 function BMI計算() { 
-var 体重 = Number(document.getElementById('weight').value); 
-var 身長 = Number(document.getElementById('height').value) / 100;
-// cmをmに変換 
-var BMI = 体重 / (身長 * 身長); alert("あなたのBMIは: " + bmi.toFixed(2) + " です"); }
+var a = Number(document.getElementById('weight').value); 
+var b = Number(document.getElementById('height').value) / 100;
+alert(a/b);
