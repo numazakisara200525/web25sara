1a0637918fbf4ab077931a0632b155f1837f2cbc
numazakisara200525 <167832970+numazakisara200525@users.noreply.github.com>
2024-12-10 10:41:18 +0900

    Create ex7-8.js

---
+++ ex7-8.js
@@ -0,0 +1,7 @@
+function nazo78() {
+if (document.getElementById('answer78-1').value == '夢') {
+alert('あたり！');} 
+else if(document.getElementById('answer78-2').value == 'ゆめ'){
+alert('あたり！');} 
+else {alert('はずれ！');}
+}
