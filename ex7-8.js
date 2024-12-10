5a498dd92fe8f388f2c4ecdade2e183fbc50db9e
numazakisara200525 <167832970+numazakisara200525@users.noreply.github.com>
2024-12-10 10:42:50 +0900

    Update and rename ex7-8.js to ex7-9.js

--- ex7-8.js
+++
@@ -1,7 +0,0 @@
-function nazo78() {
-if (document.getElementById('answer78-1').value == '夢') {
-alert('あたり！');} 
-else if(document.getElementById('answer78-2').value == 'ゆめ'){
-alert('あたり！');} 
-else {alert('はずれ！');}
-}
