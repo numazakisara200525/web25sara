db3d83b69a733bafd262581565a8fb587aab5494
numazakisara200525 <167832970+numazakisara200525@users.noreply.github.com>
2024-12-10 09:30:35 +0900

    Update ex7-3.js

--- ex7-3.js
+++ ex7-3.js
@@ -1,4 +1,4 @@
 function BMI計算() { 
 var a = Number(document.getElementById('weight').value); 
-var b = Number(document.getElementById('height').value) / 100;
-alert(a/b);
+var b = Number(document.getElementById('height').value):
+alert(a/(b*b));
