e48da30684061c498dae3e99e84e6ff55ff98cb2
numazakisara200525 <167832970+numazakisara200525@users.noreply.github.com>
2024-12-10 10:30:39 +0900

    Update ex7-7.js

--- ex7-7.js
+++ ex7-7.js
@@ -1,5 +1,12 @@
-function nazo77() {
+//練習問題7-7
+/* function nazo77() {
 if (document.getElementById('answer77').value = '夢') {
+alert('あたり！');} 
+else {alert('はずれ！');}
+} */
+//練習問題7-8
+function nazo77() {
+if (document.getElementById('answer77').value == '夢') {
 alert('あたり！');} 
 else {alert('はずれ！');}
 }
