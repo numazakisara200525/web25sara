400abd96fc15bff902f499472a62dac961ac83c1
numazakisara200525 <167832970+numazakisara200525@users.noreply.github.com>
2024-12-17 10:05:37 +0900

    Update ex7-11.js

--- ex7-11.js
+++ ex7-11.js
@@ -2,7 +2,5 @@ function uru() {
 var seireki =Number(document.getElementById('tosi').value); 
 var uruu =(seireki/4)
 var heinen=(seireki%4)
-{
-alert('うるう年');} 
-else {alert('平年');}
-}
+if (heinen=0) { alert("閏年"); } 
+if (heinen>0) { alert("平年"); }
