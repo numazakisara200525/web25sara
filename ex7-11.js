d0d9bf52b1b54d3eecebda3636a2e6f99feca649
numazakisara200525 <167832970+numazakisara200525@users.noreply.github.com>
2024-12-17 09:54:53 +0900

    Update ex7-11.js

--- ex7-11.js
+++ ex7-11.js
@@ -4,10 +4,3 @@ if (document.getElementById('uru').value == '夢') {
 alert('うるう年');} 
 else {alert('平年');}
 }
-function bmi() { 
-var a = Number(document.getElementById('weight2').value); 
-var b = Number(document.getElementById('height2').value);
-var bmiValue =(a/(b*b/100)*100);
-if (bmiValue < 18.5) { alert("低体重：" + bmiValue); } 
-else if (bmiValue < 25) { alert("普通：" + bmiValue); } 
-else { alert("肥満：" + bmiValue); } }
