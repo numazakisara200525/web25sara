c2f1bbb09631a8fd1365e2ed415efaa9de75a1f8
numazakisara200525 <167832970+numazakisara200525@users.noreply.github.com>
2024-12-17 09:47:05 +0900

    Update ex7-11.js

--- ex7-11.js
+++ ex7-11.js
@@ -1,7 +1,5 @@
 function nazo79() {
-if (document.getElementById('answer79').value == '夢') {
-alert('あたり！');} 
-else if(document.getElementById('answer79').value == 'ゆめ'){
-alert('あたり！');} 
-else {alert('はずれ！');}
+if (document.getElementById('uru').value == '夢') {
+alert('うるう年');} 
+else {alert('平年');}
 }
